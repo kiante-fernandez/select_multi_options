ef5c3112b27bade2082a93ca4fbf244457ea8ff3
Kiante Fernandez <kiante27@yahoo.com>
2021-12-01 14:53:01 -0500

    moved experiement to its own file

--- js/jspsych/experiment.js
+++ js/jspsych/experiment.js
@@ -1,65 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title> Select Multi-Opions Experiment </title>
-
-    <script src="jspsych/jspsych.js"></script>
-    <script src="jspsych/plugin-html-button-response.js"></script>
-    <script src="jspsych/plugin-instructions.js"></script>
-    <script src="jspsych/plugin-image-keyboard-response.js"></script>
-    <script src="jspsych/plugin-html-keyboard-response.js"></script>
-    <script src="jspsych/plugin-preload.js"></script>
-
-    <script src="plugin-multi_select/plugin-select-multiple-buttons.js"></script>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/6.2.1/math.js"></script>
-
-    <script src="jspsych/plugin-webgazer-calibrate.js"></script>
-    <script src="jspsych/plugin-webgazer-init-camera.js"></script>
-    <script src="jspsych/plugin-webgazer-validate.js"></script>
-    <script src="jspsych/extension-webgazer.js"></script>
-    <script src="node_modules/webgazer/dist/webgazer.js"></script>
-
-    <link href="css/jspsych.css" rel="stylesheet" type="text/css" />
-
-    <style>
-
-    .jspsych-multiple-select-response-button {
-        float: center;
-        border: 4px
-        solid #FFFFFF;
-        cursor: pointer;
-        width: 130px;
-        padding: 1px;
-        margin-bottom: 10px;
-    }
-    .active {
-        border: 4px
-        solid #FFFF00;
-        background: #eeeeee;
-    }
-    /* Clear floats after image containers */
-    .jspsych-multiple-select-response-button::after {
-        content: "";
-        clear: both;
-        display: table;
-    }
-    </style>
-
-</head>
-
-<body>
-</body>
-
-<script>
 var jsPsych = initJsPsych({
     on_finish: function () {
+        var csv = jsPsych.data.get().csv();
+        var filename = jsPsych.data.get().values()[0].subject_id + "_" + DATE + ".csv";;
+        downloadCSV(csv,filename);
         jsPsych.data.displayData()
     }
 }
 );
+
+// CONSTANTS
+var TODAY = new Date();
+var DD = String(TODAY.getDate()).padStart(2, '0');
+var MM = String(TODAY.getMonth() + 1).padStart(2, '0');
+var YYYY = TODAY.getFullYear();
+const DATE = YYYY + MM + DD;
 
 //Creating list of image paths to load.
 //These are image paths to images for the experimental treatment shown on each trial
@@ -75,23 +29,86 @@ for (var i = 0; i < arrayLength; i++) {
     var path_str1 = "img/food/food" + String(image_indices_lst[i]) + ".jpg";
     image_paths.push(path_str1);
 }
+// Randomly select a subset of images
+const shuffled_image_names = jsPsych.randomization.shuffle(image_paths);
+// Get the img paths for the selected images
+const trial_options = shuffled_image_names.slice(0, 15);
 
 // Preloading files are needed to present the stimuli accurately.
 const preload = {
     type: jsPsychPreload,
     images: image_paths,
 }
-const shuffled_image_names = jsPsych.randomization.shuffle(image_paths);
-const trial_options = shuffled_image_names.slice(0, 15);
 
-var factors = {
-    image: shuffled_image_names,
+// Welcome
+var welcome_block = {
+    data: {
+        screen_id: "welcome"
+    },
+    type: jsPsychSurveyHtmlForm,
+    preamble: "<p>Welcome to the experiment!</p>"+
+    "Please complete the form",
+    html: "<p>Participant ID: <input name = 'subject_id' type = 'text'/></p>",
+    on_finish: function(data) {
+        responses = data.response;
+        jsPsych.data.addProperties({
+            subject_id: responses.subject_id,
+            ID_DATE: responses.subject_id + "_" + DATE,
+        })
+    }
 };
 
-var factorial_values = jsPsych.randomization.factorial(factors);
+// Instructions I
+var instruction_block_1 = {
+    data: {screen_id: "instructions"},
+    type: jsPsychInstructions,
+    pages: [
+        '<p>Welcome to the study!</p>' +
+        "<p>Today, you will make some decisions about food items. </p>" +
+        "<p>Please pay attention to the instructions. For part of the study, your eye movements will be tracked.</p>"+
+        "<p>If you have any questions, please ask the experimenter at this time. If you are ready to begin, please press the enter key.</p>",
+    ],
+    key_forward: ' '
+};
+// Calibration eye tracking
 
+
+// Experiment instructions
+// here we need to choose one of two frames randomly. Then we need to show the approptirate text.
+var condition_instructions = [
+    //condition 1
+    "<p>You have been selected to buy a box of snacks. Each item costs its normal price.</p>" +
+    "<p>All the items available to buy are in your box.</p>" +
+    "<p>Click on all the items you want to keep in your box.</p>"+
+    "<p>You will have a chance to receive each item that you selected.</p>"+
+    "<p>When you’re finished selecting items, press the space bar to finalize your selection.</p>"+
+    "<p>If you are ready to begin, please press the space bar.</p>",
+    //condition 2
+    "<p>You have been selected to buy a box of snacks. Each item costs its normal price.</p>" +
+    "<p>All the items available to buy will be shown.</p>" +
+    "<p>Click on all the items you want to add to your box.</p>"+
+    "<p>You will have a chance to receive each item that you selected.</p>"+
+    "<p>When you’re finished selecting items, press the space bar to finalize your selection.</p>"+
+    "<p>If you are ready to begin, please press the space bar.</p>"
+];
+//randomly select a condition
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+};
+const condition_choice = getRandomInt(2);
+// expected output: 0, 1
+
+//we need to get some indicator that tells us what conditoin this was
+var instruction_block_2 = {
+    data: {screen_id: "framing", condition: condition_choice},
+    type: jsPsychInstructions,
+    pages: [condition_instructions[condition_choice]
+],
+key_forward: ' '
+};
+//choice task
 var fixation = {
-    data: {screen_id: "fixation"},
+    data: {screen_id: "fixation", stimulus: "fixation"},
     type: jsPsychHtmlKeyboardResponse,
     stimulus: "<div style='font-size: 60px'>+</div>",
     trial_duration: [500],
@@ -149,19 +166,31 @@ button_html: [
         data.options_selected = matches.length
     }
 };
+// Debrief
+//we need to randomly select if this subject actully gets the foods or not
+var Debrief = {
+    data: {
+        screen_id: "Debrief"
+    },
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: "<p>You have completed the experiment.</p>" +
+    "<p>please contact the experimenter.</p>",
+    choices: ' ',
+    response_ends_trial: true
+};
 
 var trials_with_variables = {
     timeline: [fixation, trial],
-    // timeline_variables: factorial_values
 };
-
 // timeline
 var timeline = []
+
 timeline.push(preload);
+timeline.push(welcome_block);
+timeline.push(instruction_block_1);
+timeline.push(instruction_block_2);
 timeline.push(trials_with_variables);
+timeline.push(Debrief);
 
 //Start Experiment
 jsPsych.run(timeline);
-
-</script>
-</html>
