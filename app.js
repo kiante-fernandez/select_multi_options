073e246b9d6570674b84744da99ed12f69da9bed
Kiante Fernandez <kiante27@yahoo.com>
2023-05-24 15:24:07 -0400

    changes to connect to google drive

--- app.js
+++ app.js
@@ -1,24 +1,101 @@
 // --- LOAD MODULES
+const fs = require("fs");
+const fetch = import("node-fetch");
+const ejs = require("ejs");
+// const { response } = require('express');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+const path = require('path');
+const process = require('process');
+
 var express = require("express"),
     mymods = require("./scripts/mymods.js"),
     body_parser = require("body-parser");
     bowser = require("bowser"); // CommonJS
 
-var savedropbox = mymods.saveDropbox;
-var json2csv = mymods.json2csv;
-
 // --- INSTANTIATE THE APP
 var app = express();
+const subjects = {};
+
+// --- Database SETUP
+const CLIENT_ID = '228376881552-hrg29htnkq46t0t8ql91uhe4o291tkjr.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+// const REDIRECT_URL = 'https://neuroeconomics.herokuapp.com';
+const REDIRECT_URL = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//04xDNYRN9D_o7CgYIARAAGAQSNwF-L9IrvNi5g6WVtIprjCF36ey835OvEeer71-lwCFvQVL6EAmbNLYO3tyfjtH0-AwZ9IIE-bQ';
+// const ACCESS_TOKEN = 'ya29.a0Aa4xrXNzy99TN4nngl464mzynSe4U2uPdTg3yBvEaQfgvl2DpI1rzyLRSnvxgsd2CPiq7zuZSUt4IW0XGm3gxUf_t2spUG7xAij4siTuSehg13Il0FdP3KBWJkaLzwQd81d16SssYHpsha3pMJ6JDxzE0nf_aCgYKATASARISFQEjDvL9LXmlqdTqiboWb7_a4dik-g0163'
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL
+);
+// oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN ,access_token: ACCESS_TOKEN});
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN});
+
+const drive = google.drive({ version: 'v3', auth: oauth2Client });
+
+function writeJSONFile(id,data) {
+    try {
+    const createStream = fs.createWriteStream(`similarity_exp_${id}.json`);
+    const writeStream = fs.createWriteStream(`similarity_exp_${id}.json`);
+    writeStream.write(JSON.stringify(data));
+    createStream.end();
+    writeStream.end();
+    } catch (err){
+        throw err;
+    }
+}
+
+async function removeFile(id) {
+    try {
+    await fs.unlink(`similarity_exp_${id}.json`);
+      console.log('filePath was deleted');
+    } catch(error) {
+        console.error(`Got an error trying to delete the file: ${error.message}`);
+    }
+};
+async function uploadFile(id,data) {
+    try{
+    await writeJSONFile(id,data);
+    // console.log("are we breaking?")
+    const response = await drive.files.create({
+            requestBody: {
+                name: `similarity_exp_${id}.json`, //file name
+                mimeType: 'application/json',
+                parents: ['1PFHm5wI7hOz4eu1gRppwtgVFZkl5M_tk']
+            },
+            media: {
+                mimeType: 'application/json',
+                body: fs.createReadStream(`similarity_exp_${id}.json`),
+            },
+        });
+        // report the response from the request
+        console.log(response.data);
+       // await removeFile(id);
+    }catch (error) {
+        //report the error message
+        console.log(error.message);
+    }
+}
 
 // --- STATIC MIDDLEWARE
 app.use(express.static(__dirname + '/public'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/scripts", express.static(__dirname + '/scripts'));
-app.use(body_parser.json());
 
+// --- BODY PARSING MIDDLEWARE
+// app.use(body_parser.json());// to support JSON-encoded bodies
+
+// --- need more memory?
+app.use(body_parser.json({limit: '50mb'}));
+app.use(body_parser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json());
 
 // --- VIEW LOCATIONS. SET UP SERVING STATIC HTML
 app.set("views", __dirname + "/public/views");
+app.set('img', __dirname + '/public/img');
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
@@ -36,22 +113,12 @@ app.post('/experiment-data', function(request, response) {
     data = request.body;
     id = data[0].subject_id;
     id = id.replace(/'/g, "");
-    var currentdate = new Date();
-    ID_DATE = data[0].ID_DATE;
-    filename = ID_DATE + ".json";
-    foldername = id;
+    // var currentdate = new Date();
+    // ID_DATE = data[0].ID_DATE;
+    // filename = ID_DATE + ".json";
+    // foldername = id;
     data = JSON.stringify(data);
-    savedropbox(data, filename, foldername);
-
-    // convert json to csv (just convert later)
-    // DATA_CSV = json2csv(request.body);
-    // Get filename from data
-    // var rows = DATA_CSV.split("\n");
-    // ID_DATE_index = rows[0].split(",").indexOf('"ID_DATE"');
-    // ID_DATE = rows[1].split(",")[ID_DATE_index];
-    // ID_DATE = ID_DATE.replace(/"/g, "");
-    // filename = ID_DATE + ".csv";
-    // savedropbox(DATA_CSV, filename);
+    uploadFile(id,data);
 
     response.end();
     // console.log(DATA_CSV)
--- scripts/mymods.js
+++ scripts/mymods.js
@@ -1,16 +1,51 @@
-require("dotenv").config();
-var Dropbox = require("dropbox").Dropbox;
-const fetch = require("node-fetch");
+// require("dotenv").config();
+// var Dropbox = require("dropbox").Dropbox;
+// const fetch = require("node-fetch");
 
-const dbx = new Dropbox({
-    accessToken: process.env.DROPBOXACCESSTOKEN,
-    fetch
-});
+// const dbx = new Dropbox({
+//     accessToken: process.env.DROPBOXACCESSTOKEN,
+//     fetch
+// });
 
-// saveDropbox = function(content, filename) {
-//     dbx.filesUpload({
+// // saveDropbox = function(content, filename) {
+// //     dbx.filesUpload({
+// //         path: "/" + filename,
+// //         contents: content
+// //     }).then(function() {
+// //         console.log("Completed");
+// //     })
+// //         .catch(function(error) {
+// //             console.error(error);
+// //         });
+// // };
+
+// saveDropbox = function (content, filename, foldername) {
+//     return dbx.filesGetMetadata({
+//         path: "/" + foldername,
+//     }).catch(err => {
+//         //      console.log(err['error']['path'])
+//         if (err.error.error.path['.tag'] == 'not_found') {
+//             return dbx.filesCreateFolder({
+//                 path: "/" + foldername,
+//                 autorename: false,
+//             });
+//         } else {
+//             throw err;
+//         }
+//     }).then(() => {
+//         return dbx.filesUpload({
+//             path: "/" + foldername + "/" + filename,
+//             contents: content
+//         });
+//     });
+// };
+
+// saveDropboxSingleFile = function (content, filename) {
+//     return dbx.filesUpload({
 //         path: "/" + filename,
-//         contents: content
+//         contents: content,
+//         autorename: false,
+//         mode:  'overwrite'
 //     }).then(function() {
 //         console.log("Completed");
 //     })
@@ -19,79 +54,44 @@ const dbx = new Dropbox({
 //         });
 // };
 
-saveDropbox = function (content, filename, foldername) {
-    return dbx.filesGetMetadata({
-        path: "/" + foldername,
-    }).catch(err => {
-        //      console.log(err['error']['path'])
-        if (err.error.error.path['.tag'] == 'not_found') {
-            return dbx.filesCreateFolder({
-                path: "/" + foldername,
-                autorename: false,
-            });
-        } else {
-            throw err;
-        }
-    }).then(() => {
-        return dbx.filesUpload({
-            path: "/" + foldername + "/" + filename,
-            contents: content
-        });
-    });
-};
+// function json2csv(objArray){
+//     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+//     var line = '';
+//     var result = '';
+//     var columns = [];
 
-saveDropboxSingleFile = function (content, filename) {
-    return dbx.filesUpload({
-        path: "/" + filename,
-        contents: content,
-        autorename: false,
-        mode:  'overwrite'
-    }).then(function() {
-        console.log("Completed");
-    })
-        .catch(function(error) {
-            console.error(error);
-        });
-};
+//     var i = 0;
+//     for (var j = 0; j < array.length; j++) {
+//         for (var key in array[j]) {
+//             var keyString = key + "";
+//             keyString = '"' + keyString.replace(/"/g, '""') + '",';
+//             if (!columns.includes(key)) {
+//                 columns[i] = key;
+//                 line += keyString;
+//                 i++;
+//             }
+//         }
+//     }
 
-function json2csv(objArray){
-    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    var line = '';
-    var result = '';
-    var columns = [];
+//     line = line.slice(0, -1);
+//     result += line + '\r\n';
 
-    var i = 0;
-    for (var j = 0; j < array.length; j++) {
-        for (var key in array[j]) {
-            var keyString = key + "";
-            keyString = '"' + keyString.replace(/"/g, '""') + '",';
-            if (!columns.includes(key)) {
-                columns[i] = key;
-                line += keyString;
-                i++;
-            }
-        }
-    }
+//     for (var i = 0; i < array.length; i++) {
+//         var line = '';
+//         for (var j = 0; j < columns.length; j++) {
+//             var value = (typeof array[i][columns[j]] === 'undefined') ? '' : array[i][columns[j]];
+//             var valueString = value + "";
+//             line += '"' + valueString.replace(/"/g, '""') + '",';
+//         }
 
-    line = line.slice(0, -1);
-    result += line + '\r\n';
+//         line = line.slice(0, -1);
+//         result += line + '\r\n';
+//     }
 
-    for (var i = 0; i < array.length; i++) {
-        var line = '';
-        for (var j = 0; j < columns.length; j++) {
-            var value = (typeof array[i][columns[j]] === 'undefined') ? '' : array[i][columns[j]];
-            var valueString = value + "";
-            line += '"' + valueString.replace(/"/g, '""') + '",';
-        }
+//     return result;
 
-        line = line.slice(0, -1);
-        result += line + '\r\n';
-    }
+// };
 
-    return result;
-
-};
-
-module.exports.saveDropbox = saveDropbox;
-module.exports.saveDropboxSingleFile = saveDropboxSingleFile;
-module.exports.json2csv = json2csv;
+// module.exports.saveDropbox = saveDropbox;
+// module.exports.saveDropboxSingleFile = saveDropboxSingleFile;
+// module.exports.json2csv = json2csv;
